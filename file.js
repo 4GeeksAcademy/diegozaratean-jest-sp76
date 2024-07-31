58c76aa88f37fa59baf24a82588ed2b37224d6db
diego <diegozaratean@gmail.com>
2024-07-31 19:21:01 +0000

    finish react 1 class

---
+++ file.js
@@ -0,0 +1,7 @@
+import { sum  } from './file2.js'
+import unique from './file2.js';
+console.log(sum(2,3));
+
+// console.log(substraction(3,3))
+
+console.log(unique(5,9))
---
+++ file2.js
@@ -0,0 +1,19 @@
+// crear funcion suma
+let sum = (a,b)=>{
+    return a + b
+};
+
+let substraction = (a,b)=>{
+    return a - b
+};
+
+let unique = (a,b)=>{
+    return a * b
+};
+
+export {
+    sum,
+    substraction
+};
+
+export default unique;
